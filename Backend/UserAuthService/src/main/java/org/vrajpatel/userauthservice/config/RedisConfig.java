daa3e21bfc8b08d133e5a79c89d59a25a92197eb
Vraj Patel <vraj.patel.12390@gmail.com>
2025-08-05 10:10:48 -0400

    Update redis config

--- Backend/UserAuthService/src/main/java/org/vrajpatel/userauthservice/config/RedisConfig.java
+++ Backend/UserAuthService/src/main/java/org/vrajpatel/userauthservice/config/RedisConfig.java
@@ -1,24 +1,26 @@
-package org.vrajpatel.userauthservice.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-
-@Configuration
-public class RedisConfig {
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        return redisTemplate;
-    }
-
-
-}
+//package org.vrajpatel.userauthservice.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+////
+////@Configuration
+////public class RedisConfig {
+////
+////    @Bean
+////    public JedisConnectionFactory jedisConnectionFactory() {
+////        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+////        jedisConnectionFactory.getClusterConfiguration();
+////        return jedisConnectionFactory;
+////    }
+////
+////    @Bean
+////    public RedisTemplate<String, Object> redisTemplate() {
+////        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+////        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+////        return redisTemplate;
+////    }
+////
+////
+////}
