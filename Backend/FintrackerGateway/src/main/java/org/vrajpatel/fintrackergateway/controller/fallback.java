7701e3e97007b633cc85b5a3eb6d3c515de725c0
Vraj Patel <vraj.patel.12390@gmail.com>
2025-09-12 12:45:00 -0400

    Added the Fallback in case of service unavailaibility

--- Backend/FintrackerGateway/src/main/java/org/vrajpatel/fintrackergateway/controller/fallback.java
+++ Backend/FintrackerGateway/src/main/java/org/vrajpatel/fintrackergateway/controller/fallback.java
@@ -3,13 +3,15 @@ package org.vrajpatel.fintrackergateway.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class fallback {
 
     @RequestMapping("/fallback")
-    public ResponseEntity<String> fallback() {
+    public ResponseEntity<String> getFallback() {
+
         return ResponseEntity.ok("fallback");
     }
 }
