543a7b61fe5ca8dd63b7345a57ecba0e5a47da28
Vraj Patel <vraj.patel.12390@gmail.com>
2025-10-15 18:29:29 -0400

    Removed the StoryBook and Debugged the Linting Issues

--- Frontend/fintracker-frontend/src/Component/personalexpense/AddExpense.tsx
+++ Frontend/fintracker-frontend/src/Component/personalexpense/AddExpense.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 function classNames(...c: (string | false | null | undefined)[]) {
--- Frontend/fintracker-frontend/vite.config.ts
+++ Frontend/fintracker-frontend/vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 import mkcert from 'vite-plugin-mkcert';
 // https://vite.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -21,21 +18,7 @@ export default defineConfig({
       plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
+      ]
     }]
   }
 });
